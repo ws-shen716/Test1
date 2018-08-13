eaff446bbf2605333627f016b3b2aefc7f08090f
shen <2511327517@qq.com>
2018-08-13 21:36:28 +0800

    first

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = in.nextInt();
+		String str = "";
+		for (int i = a; i <= b; i++) {
+			if (isWS(i))
+				str += i + " ";
+		}
+		if (str == "") {
+			System.out.println("NIL");
+		} else {
+			str = str.substring(0, str.length() - 1);
+			System.out.println(str);
+		}
+	}
+
+	static boolean isWS(int n) { 
+		int a = 0;
+		for (int i = 1; i <= n / 2; i++) {
+			if (n % i == 0) {
+				a += i;
+			}
+		}
+		if (a == n)
+			return true;
+		else
+			return false;
+	}
+}
